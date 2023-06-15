edccc73cdb46062fec5c7d5fcea7bdc0a0730e3c
M <monacuaulma@gmail.com>
2023-06-14 23:13:29 -0400

    Roles creation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
 import "express-async-errors";
-import auth from "./routes/auth.js";
 import dotenv from "dotenv";
+import db from "./models/index.js";
+import { MongoClient } from "mongodb";
 
 dotenv.config();
+
+const connectionString = process.env.COSMOS_CONNECTION_STRING || "";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -12,7 +15,32 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", auth);
+const Role = db.role;
+db.mongoose
+	.connect(connectionString)
+	.then(() => {
+		console.log("Successfully connect to MongoDB.");
+		initial();
+	})
+	.catch((err) => {
+		console.error("Connection error", err);
+		process.exit();
+	});
+
+const initial = () => {
+	const roleUser = new Role({
+		name: "user",
+	});
+
+	roleUser
+		.save()
+		.then(() => {
+			console.log("added 'user' to roles collection");
+		})
+		.catch((error) => {
+			console.error("error", error);
+		});
+};
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port: ${PORT}`);
---
+++ src/models/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import user from "./user.js";
+import role from "./role.js";
+mongoose.Promise = global.Promise;
+
+const db = {};
+
+db.mongoose = mongoose;
+
+db.user = user;
+db.role = role;
+
+db.ROLES = ["user", "admin", "moderator"];
+
+export default db;

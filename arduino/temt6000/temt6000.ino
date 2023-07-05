d916094df840433a51f0f2be9fbd86ea3007163f
Luisa López <lmlopez.19@est.ucab.edu.ve>
2023-07-05 13:57:39 -0400

    Add voltages to temt6000

--- arduino/temt6000/temt6000.ino
+++ arduino/temt6000/temt6000.ino
@@ -5,6 +5,7 @@ SoftwareSerial espSerial(6, 5);
 int temt6000Pin = A0;
 float light;
 int light_value;
+float volt;
 
 #define outPin 6
 
@@ -23,12 +24,15 @@ void loop() {
   int light_value = analogRead(temt6000Pin);
   light = light_value * 0.0976;  // percentage calculation
 
+  //volt conversion
+  int mVolt = map(light_value, 0, 1023, 0, 5000); 
+  volt=(double)mVolt/1000;
 
-  if (light <= 1.0) {
-    str = String("OFF. ") + String(light) + String("%");
+  if (light <= 1.0 || volt <=0) {
+    str = String("OFF. ") + String(light) + String("% ")+String(volt,3)+String("V");
     Serial.println(str);
-  } else {
-    str = String("ON. ") + String(light) + String("%");
+  } else if (light > 1.0 || volt > 0){
+    str = String("ON. ") + String(light) + String("% ")+String(volt,3)+String("V");
     Serial.println(str);
   }
 

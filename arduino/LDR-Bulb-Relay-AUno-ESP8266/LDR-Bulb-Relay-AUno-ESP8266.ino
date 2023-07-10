1700d428e10dc9e8918790b61ba4715d808b2b56
Luisa López <lmlopez.19@est.ucab.edu.ve>
2023-07-10 12:21:43 -0400

    DHT21 and HiveMQ connection done

--- arduino/LDR-Bulb-Relay-AUno-ESP8266/LDR-Bulb-Relay-AUno-ESP8266.ino
+++ arduino/LDR-Bulb-Relay-AUno-ESP8266/LDR-Bulb-Relay-AUno-ESP8266.ino
@@ -7,6 +7,8 @@
 #include <FS.h>;
 #include <LittleFS.h>;
 #include <CertStoreBearSSL.h>;
+#include <ArduinoJson.h>;
+
 
 
 //wifi
@@ -24,18 +26,50 @@ const int mqtt_port = 8883;
 const int mqtt_websocket_port = 8884;
 long writeChannelID = 86;
 
-// A single, global CertStore which can be used by all connections.
-// Needs to stay live the entire time any of the WiFiClientBearSSLs
-// are present.
-BearSSL::CertStore certStore;
-
+/**** Secure WiFi Connectivity Initialisation *****/
 WiFiClientSecure espClient;
-PubSubClient* client;
-unsigned long lastMsg = 0;
-#define MSG_BUFFER_SIZE (500)
-char msg[MSG_BUFFER_SIZE];
-int value = 0;
 
+/**** MQTT Client Initialisation Using WiFi Connection *****/
+PubSubClient client(espClient);
+
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE (50)
+char msg[MSG_BUFFER_SIZE];
+int value=0;
+
+static const char* root_ca PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
+TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
+WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
+ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
+MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
+h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
+0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
+A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
+T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
+B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
+B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
+KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
+OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
+jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
+qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
+rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
+HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
+hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
+ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
+3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
+NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
+ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
+TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
+jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
+oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
+4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
+mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
+emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+-----END CERTIFICATE-----
+)EOF";
 
 
 SoftwareSerial NodeMCU_SS(D1, D2);
@@ -44,20 +78,21 @@ char c;
 String dataIn;
 
 void setup_wifi() {
-  delay(100);
-  // We start by connecting to a WiFi network
+  delay(10);
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-//delay(100);
-  int i = 0;
-  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
-    delay(10000);
-    Serial.print(++i); Serial.print(' ');
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(5000);
+    Serial.print(".");
   }
+
+  randomSeed(micros());
+
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
@@ -84,47 +119,48 @@ void setDateTime() {
   Serial.printf("%s %s", tzname[0], asctime(&timeinfo));
 }
 
+/***** Call back Method for Receiving MQTT messages *****/
+
 void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
+
+
+  String incommingMessage = "";
+
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
+    incommingMessage += (char)payload[i];
   }
-  Serial.println();
+  Serial.println("Message arrived [" + String(topic) + "]" + incommingMessage);
 
-  // Switch on the LED if the first character is present
-  if ((char)payload[0] != NULL) {
-    //digitalWrite(LED_BUILTIN, LOW); // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is active low on the ESP-01)
-    Serial.println("The fist character is present");
-    delay(500);
-    Serial.println("The fist character is NOT present ANYMORE");
-    // digitalWrite(LED_BUILTIN, HIGH); // Turn the LED off by making the voltage HIGH
+  //check incoming message
+  if (strcmp(topic, "Bombillo/Lab. Prototipos") == 0) {
+    if (incommingMessage.equals("ON")) {
+      Serial.println("From broker: Bulb ON");
+    } else if (incommingMessage.equals("OFF")) {
+      Serial.println("From broker: Bulb OFF");
+    }
   } else {
-    Serial.println("The fist character is NOT present");
-    // digitalWrite(LED_BUILTIN, HIGH); // Turn the LED off by making the voltage HIGH
+    Serial.println("Nothing");
   }
 }
 
 
 void reconnect() {
   // Loop until we’re reconnected
-  while (!client->connected()) {
+  while (!client.connected()) {
     Serial.print("Attempting MQTT connection…");
     String clientId = mqtt_clientid;
     // Attempt to connect
     // Insert your password
-    if (client->connect(clientId.c_str(), mqtt_username, mqtt_password)) {
+    if (client.connect(clientId.c_str(), mqtt_username, mqtt_password)) {
       Serial.println("connected");
-      // Once connected, publish an announcement…
-      client->publish("testTopic", "hello world");
-      // … and resubscribe
-      client->subscribe("testTopic");
+      // Once connected, subscribe the topic
+      client.subscribe("Bombillo/Lab. Prototipos");
+
+
     } else {
       Serial.print("failed, rc = ");
-      Serial.print(client->state());
+      Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
       // Wait 5 seconds before retrying
       delay(5000);
@@ -132,39 +168,35 @@ void reconnect() {
   }
 }
 
+
+/**** Method for Publishing MQTT Messages **********/
+void publishMessage(const char* topic, String payload, boolean retained) {
+  if (client.publish(topic, payload.c_str(), true))
+    Serial.println("Message publised [" + String(topic) + "]: " + payload);
+}
+
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
 
 
- // delay(500);
+  delay(500);
 
-    LittleFS.begin();
-    setup_wifi();
-  // setDateTime();
+  LittleFS.begin();
+  setup_wifi();
+#ifdef ESP8266
+  espClient.setInsecure();
+#else
+  espClient.setCACert(root_ca);  // enable this line and the the "certificate" code for secure connection
+#endif
 
- int numCerts = certStore.initCertStore(LittleFS, PSTR("/certs.idx"), PSTR("/certs.ar"));
-  Serial.printf("Number of CA certs read: %d\n", numCerts);
-  if (numCerts == 0) {
-    Serial.printf("No certs found. Did you run certs-from-mozilla.py and upload the LittleFS directory before running?\n");
-    return; // Can't connect to anything w/o certs!
-  }
-
-  BearSSL::WiFiClientSecure *bear = new BearSSL::WiFiClientSecure();
-  // Integrate the cert store with this connection
-  bear->setCertStore(&certStore);
-
-  client = new PubSubClient(*bear);
-
-
-    client->setServer(mqtt_server, mqtt_port);
-    client->setCallback(callback);
-
-
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
   NodeMCU_SS.begin(115200);
 }
 
 
 void loop() {
+
   while (NodeMCU_SS.available() > 0) {
     c = NodeMCU_SS.read();
     if (c == '\n') {
@@ -172,40 +204,56 @@ void loop() {
     } else {
       dataIn += c;
     }
+
+    // if (!client.connected()) {
+    //   reconnect();
+    // }
+    // client.loop();
+
+   
   }
-
-  if (!client->connected()) {
-    reconnect();
-  }
-  client->loop();
-
- //To serial monitor
-  // if (c == '\n') {   
-  //   String ack=String("Recibido")+String('\n'); 
-  //   NodeMCU_SS.print(ack);
-  //   Serial.println(dataIn);
-  //   c = 0;
-  //   dataIn = "";
-  // }
+  //  //To serial monitor
+  //    if (c == '\n') {
+  //     String ack=String("Recibido")+String('\n');
+  //     NodeMCU_SS.print(ack);
+  //     Serial.println(dataIn);
+  //     c = 0;
+  //     dataIn = "";
+  //   }
 
 
- // To Broker
-  if (c == '\n') {
-    Serial.println(dataIn);
-  String ack=String("Recibido")+String('\n'); 
-    NodeMCU_SS.print(ack);
-    unsigned long now = millis();
-    if (now - lastMsg > 2000) {
-      lastMsg = now;
-      ++value;
-      snprintf(msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
-      Serial.print("Publish message: ");
-      Serial.println(msg);
-      static char s_dataIn[50];
-      client->publish("Bombillo/Lab. Prototipos", dataIn.c_str());
+    // To Broker
+    if (c == '\n') {
+      Serial.println(dataIn);
+      String ack = String("Recibido") + String('\n');
+      NodeMCU_SS.print(ack);
+
+      DynamicJsonDocument doc(1024);
+
+      doc["deviceId"] = "NodeMCU";
+      doc["siteId"] = "My Demo Lab";
+      doc["bulbState"] = dataIn;
+
+      char mqtt_message[128];
+      serializeJson(doc, mqtt_message);
+
+    Serial.println(mqtt_message);
+     publishMessage("Bombillo/Lab. Prototipos", mqtt_message, true);
+
+      // unsigned long now = millis();
+      // if (now - lastMsg > 2000) {
+      //   lastMsg = now;
+      //   // ++value;
+      //   // snprintf(msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
+      //   // Serial.print("Publish message: ");
+      //   // Serial.println(msg);
+      //     Serial.println("Publicando...");
+
+      //   static char s_dataIn[50];
+      //   client.publish("Bombillo/Lab. Prototipos", dataIn.c_str());
+      // }
+      c = 0;
+      dataIn = "";
     }
-    c = 0;
-    dataIn = "";
-  }
-
+   
 }
--- arduino/dht21-hivemq/dht21-hivemq.ino
+++ arduino/dht21-hivemq/dht21-hivemq.ino
@@ -1,41 +1,82 @@
-#include <ESP8266WiFi.h>
+#ifdef ESP8266
+ #include <ESP8266WiFi.h>
+ #else
+ #include <WiFi.h>
+#endif
+
+#include <DHT.h>
+#include <ArduinoJson.h>
 #include <PubSubClient.h>
-#include <time.h>
-#include <TZ.h>
-#include <FS.h>
-#include <LittleFS.h>
-#include <CertStoreBearSSL.h>
-#include <DHT.h>  // DHT library 1.2.3 (newer library returns error)
+#include <WiFiClientSecure.h>
+
 
 #define DHTTYPE DHT21  // dht type of sensor, change if you have other
 #define DHTPIN 14      // D5 pin on nodeMCU board, GPIO pin 14 - connected DATA from sensor
+
+
+/**** LED Settings *******/
+const int led = 5; //Set LED pin as GPIO5
 
 // wifi
 const char* ssid = "luisa";
 const char* password = "123luisa";
 
 // MQTT Server
-const char* mqtt_server = "17363ff3932e49388d23181ed491867a.s2.eu.hivemq.cloud";
+const char* mqtt_server = "6f73b3569de64e8c94164637cdc7a301.s2.eu.hivemq.cloud";
 const char* topicT = "channels/2207179/publish/fields/field1";
 const char* topicH = "channels/2207179/publish/fields/field2";
-const char* mqtt_clientid = "luisamlopez";
-const char* mqtt_username = "luisamlopez";
-const char* mqtt_password = "279480412.Lm";
+const char* mqtt_clientid = "ucontrol";
+const char* mqtt_username = "ucontrol";
+const char* mqtt_password = "Ucontrol123";
 const int mqtt_port = 8883;
 const int mqtt_websocket_port = 8883;
 long writeChannelID = 86;
 
-// A single, global CertStore which can be used by all connections.
-// Needs to stay live the entire time any of the WiFiClientBearSSLs
-// are present.
-BearSSL::CertStore certStore;
-
+/**** Secure WiFi Connectivity Initialisation *****/
 WiFiClientSecure espClient;
-PubSubClient* client;
+
+/**** MQTT Client Initialisation Using WiFi Connection *****/
+PubSubClient client(espClient);
+
 unsigned long lastMsg = 0;
-#define MSG_BUFFER_SIZE (500)
+#define MSG_BUFFER_SIZE (50)
 char msg[MSG_BUFFER_SIZE];
-int value = 0;
+
+/****** root certificate *********/
+
+static const char *root_ca PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
+TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
+WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
+ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
+MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
+h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
+0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
+A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
+T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
+B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
+B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
+KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
+OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
+jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
+qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
+rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
+HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
+hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
+ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
+3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
+NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
+ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
+TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
+jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
+oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
+4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
+mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
+emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+-----END CERTIFICATE-----
+)EOF";
 
 
 // Sensor
@@ -48,12 +89,10 @@ float temp;  //Stores temperature value
 static char s_temp[7];
 static char s_hum[7];
 
-
+/************* Connect to WiFi ***********/
 void setup_wifi() {
   delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
+  Serial.print("\nConnecting to ");
   Serial.println(ssid);
 
   WiFi.mode(WIFI_STA);
@@ -63,127 +102,90 @@ void setup_wifi() {
     delay(500);
     Serial.print(".");
   }
-
   randomSeed(micros());
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
+  Serial.println("\nWiFi connected\nIP address: ");
   Serial.println(WiFi.localIP());
 }
 
-
-void setDateTime() {
-  // You can use your own timezone, but the exact time is not used at all.
-  // Only the date is needed for validating the certificates.
-  configTime(TZ_America_Caracas, "pool.ntp.org", "time.google.com");
-
-  Serial.print("Waiting for NTP time sync: ");
-  time_t now = time(nullptr);
-  while (now < 8 * 3600 * 2) {
-    delay(100);
-    Serial.print(".");
-    now = time(nullptr);
-  }
-  Serial.println();
-
-  struct tm timeinfo;
-  gmtime_r(&now, &timeinfo);
-  Serial.printf("%s %s", tzname[0], asctime(&timeinfo));
-}
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-
-  // Switch on the LED if the first character is present
-  if ((char)payload[0] != NULL) {
-    //digitalWrite(LED_BUILTIN, LOW); // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is active low on the ESP-01)
-    Serial.println("The fist character is present");
-    delay(500);
-    Serial.println("The fist character is NOT present ANYMORE");
-    // digitalWrite(LED_BUILTIN, HIGH); // Turn the LED off by making the voltage HIGH
-  } else {
-    Serial.println("The fist character is NOT present");
-    // digitalWrite(LED_BUILTIN, HIGH); // Turn the LED off by making the voltage HIGH
-  }
-}
-
-
+/************* Connect to MQTT Broker ***********/
 void reconnect() {
-  // Loop until we’re reconnected
-  while (!client->connected()) {
-    Serial.print("Attempting MQTT connection…");
-    String clientId = mqtt_clientid;
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    String clientId = "ESP8266Client-";   // Create a random client ID
+    clientId += String(random(0xffff), HEX);
     // Attempt to connect
-    // Insert your password
-    if (client->connect(clientId.c_str(), mqtt_username, mqtt_password)) {
+    if (client.connect(clientId.c_str(), mqtt_username, mqtt_password)) {
       Serial.println("connected");
-      // Once connected, publish an announcement…
-      client->publish("testTopic", "hello world");
-      // … and resubscribe
-      client->subscribe("testTopic");
+
+      client.subscribe("led_state");   // subscribe the topics here
+
     } else {
-      Serial.print("failed, rc = ");
-      Serial.print(client->state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");   // Wait 5 seconds before retrying
       delay(5000);
     }
   }
 }
 
+/***** Call back Method for Receiving MQTT messages and Switching LED ****/
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  String incommingMessage = "";
+  for (int i = 0; i < length; i++) incommingMessage+=(char)payload[i];
+
+  Serial.println("Message arrived ["+String(topic)+"]"+incommingMessage);
+
+  //--- check the incomming message
+    if( strcmp(topic,"led_state") == 0){
+     if (incommingMessage.equals("1")) {
+       Serial.println("Mensaje recibido, prender led");
+     }   // Turn the LED on
+     else Serial.println("Mensaje recibido, apagar led");  // Turn the LED off
+  }
+
+}
+
+/**** Method for Publishing MQTT Messages **********/
+void publishMessage(const char* topic, String payload , boolean retained){
+  if (client.publish(topic, payload.c_str(), true))
+      Serial.println("Message published ["+String(topic)+"]: "+payload);
+}
+
 
 void setup() {
-  delay(500);
-  // When opening the Serial Monitor, select 9600 Baud
+  pinMode(led, OUTPUT); //set up LED
   Serial.begin(9600);
-  delay(500);
-
-  LittleFS.begin();
+  while (!Serial) delay(1);
   setup_wifi();
-  setDateTime();
-
-  // you can use the insecure mode, when you want to avoid the certificates
-
- // espClient->setInsecure();
-
-  // int numCerts = certStore.initCertStore(LittleFS, PSTR("/certs.idx"), PSTR("/certs.ar"));
-  // Serial.printf("Number of CA certs read: %d\n", numCerts);
-  // if (numCerts == 0) {
-  //   Serial.printf("No certs found. Did you run certs-from-mozilla.py and upload the LittleFS directory before running?\n");
-  //   return; // Can't connect to anything w/o certs!
-  // }
-
-  // BearSSL::WiFiClientSecure *bear = new BearSSL::WiFiClientSecure();
-  // // Integrate the cert store with this connection
-  // bear->setCertStore(&certStore);
-
-  // client = new PubSubClient(*bear);
-
-  client->setServer(mqtt_server, mqtt_port);
-  client->setCallback(callback);
 
   dht.begin();
+
+  #ifdef ESP8266
+    espClient.setInsecure();
+  #else
+    espClient.setCACert(root_ca);      // enable this line and the the "certificate" code for secure connection
+  #endif
+
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+  
 }
 
 void loop() {
-  if (!client->connected()) {
-    reconnect();
-  }
-  client->loop();
+  
+  
+  if (!client.connected()) reconnect(); // check if client is connected
+  client.loop();
 
-
+delay(2000);
+  
   //Read data and store it to variables hum and temp
   hum = dht.readHumidity();
   temp = dht.readTemperature();
+
+
 
   //Print temp and humidity values to serial monitor
   Serial.print("Humidity: ");
@@ -192,18 +194,37 @@ void loop() {
   Serial.print(temp);
   Serial.println(" Celsius");
 
-  // convertir un número de punto flotante (float) en una cadena de caracteres (string)
-  dtostrf(temp, 6, 2, s_temp);
-  dtostrf(hum, 6, 2, s_hum);
 
-  unsigned long now = millis();
-  if (now - lastMsg > 2000) {
-    lastMsg = now;
-    ++value;
-    snprintf(msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
-    Serial.print("Publish message: ");
-    Serial.println(msg);
-    client->publish("Temperatura Lab. Prototipos", s_temp); 
-    client->publish("Humedad Lab. Prototipos", s_hum);
+
+  DynamicJsonDocument doc(1024);
+
+  doc["deviceId"] = "NodeMCU";
+  doc["siteId"] = "My Demo Lab";
+  doc["humidity"] = hum;
+  doc["temperature"] = temp;
+
+  char mqtt_message[128];
+  serializeJson(doc, mqtt_message);
+
+  publishMessage("HumTemp/Lab. Prototipos", mqtt_message, true);
+
+  delay(5000);
+
+  
+
+
+  // // convertir un número de punto flotante (float) en una cadena de caracteres (string)
+  // dtostrf(temp, 6, 2, s_temp);
+  // dtostrf(hum, 6, 2, s_hum);
+
+  // unsigned long now = millis();
+  // if (now - lastMsg > 2000) {
+  //   lastMsg = now;
+  //   ++value;
+  //   snprintf(msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
+  //   Serial.print("Publish message: ");
+  //   Serial.println(msg);
+  //   client->publish("Temperatura Lab. Prototipos", s_temp); 
+  //   client->publish("Humedad Lab. Prototipos", s_hum);
   }
-}
+

b2f6115b45cec17841f85a1aae2bdbce12f906f1
M <monacuaulma@gmail.com>
2023-08-01 16:11:13 -0400

    Add getUsers

---
+++ src/routes/users.js
@@ -0,0 +1,43 @@
+import express from "express";
+import User from "../models/user.js";
+
+const router = express.Router();
+
+router.get("/getAllUsers", async (req, res) => {
+	try {
+		const allUsers = await User.find({});
+
+		res.status(200).json({
+			success: true,
+			message: "Todos los usuarios obtenidos exitosamente",
+			users: allUsers,
+		});
+	} catch (err) {
+		res.status(500).json({ success: false, message: err.message });
+	}
+});
+
+// Get a user by ID
+router.get("/getUserById", async (req, res) => {
+	const { id } = req.body;
+
+	try {
+		const user = await User.findById(id);
+
+		if (!user) {
+			return res
+				.status(404)
+				.json({ success: false, message: "Usuario no encontrado" });
+		}
+
+		res.status(200).json({
+			success: true,
+			message: "Usuario obtenido exitosamente",
+			data: user,
+		});
+	} catch (err) {
+		res.status(500).json({ success: false, message: err.message });
+	}
+});
+
+export default router;

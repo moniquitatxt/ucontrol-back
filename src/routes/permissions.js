e4a277eda6f0737de91c705facb40d18d9d9a2a8
M <monacuaulma@gmail.com>
2023-09-13 15:25:57 -0400

    Add get permissions

--- src/routes/permissions.js
+++ src/routes/permissions.js
@@ -91,35 +91,6 @@ router.post("/permission", async (req, res) => {
 	}
 });
 
-// Get all permissions for a space
-router.get("/getSpacePermissions", async (req, res) => {
-	const { spaceId } = req.body;
-	try {
-		const permissions = await Permission.find({ spaceId });
-		res.status(200).json({
-			success: true,
-			message: "Los permisos se han obtenido exitosamente",
-			data: permissions,
-		});
-	} catch (err) {
-		res.status(500).json({ success: false, message: err.message });
-	}
-});
-
-// Get all permissions for a user
-router.get("/getUserPermissions", async (req, res) => {
-	const { userId } = req.body;
-	try {
-		const permissions = await Permission.find({ userId });
-		res.status(200).json({
-			success: true,
-			message: "Los permisos se han obtenido exitosamente",
-			data: permissions,
-		});
-	} catch (err) {
-		res.status(500).json({ success: false, message: err.message });
-	}
-});
 // Recursive function to get all subspaces
 export const getAllSubspaces = async (spaceId) => {
 	const subspaceIds = await Space.find({ parentSpace: spaceId }).distinct(
@@ -134,6 +105,48 @@ export const getAllSubspaces = async (spaceId) => {
 
 	return allSubspaces;
 };
+
+// Get all permissions for a user
+router.get("/getUserPermissions/:userId", async (req, res) => {
+	const { userId } = req.params;
+	try {
+		const permissionSpaces = await Permission.find({ userId }).distinct(
+			"spaceId"
+		);
+
+		const permissions = await Permission.find({
+			spaceId: { $in: permissionSpaces },
+		})
+			.populate({
+				path: "userId",
+				select: "email", // Select only the user's email
+				model: User,
+			})
+			.populate({
+				path: "spaceId",
+				select: "name", // Select only the space name
+				model: Space,
+			})
+			.select("_id permission userId spaceId"); // Select the fields you need
+
+		// Step 3: Construct the desired data structure
+		const formattedPermissions = permissions.map((permission) => ({
+			id: permission._id,
+			userEmail: permission.userId.email,
+			spaceName: permission.spaceId.name,
+			type: permission.permission,
+		}));
+
+		// Step 4: Return the formatted permissions in the route
+		res.status(200).json({
+			success: true,
+			message: "Los permisos se han obtenido exitosamente",
+			data: formattedPermissions,
+		});
+	} catch (err) {
+		res.status(500).json({ success: false, message: err.message });
+	}
+});
 
 router.get("/getUserParentSpaces/:userId", async (req, res) => {
 	const { userId } = req.params;
@@ -234,64 +247,23 @@ router.get("/getUserDevices/:userId", async (req, res) => {
 		res.status(500).json({ success: false, message: err.message });
 	}
 });
-// Get all users for a space
-router.get("/getSpaceUsers", async (req, res) => {
-	const { spaceId } = req.body;
-	try {
-		const permissions = await Permission.find({ spaceId });
-		const userIds = permissions.map((permission) => permission.userId);
-		const users = await User.find({ _id: { $in: userIds } });
-		res.status(200).json({
-			success: true,
-			message: "Usuarios obtenidos exitosamente",
-			data: users,
-		});
-	} catch (err) {
-		res.status(500).json({ success: false, message: err.message });
-	}
-});
 
-// Update a permission for a space and user
-router.put("/updatePermission", async (req, res) => {
-	const { spaceId, userId } = req.params;
+// Delete a permission for a space and user
+router.delete("/deletePermission", async (req, res) => {
+	const { id } = req.body;
 	try {
-		const permission = await Permission.findOneAndUpdate(
-			{ spaceId, userId },
-			req.body,
-			{ new: true }
-		);
+		const permission = await Permission.findByIdAndDelete(id);
 		if (!permission) {
 			throw new Error("Permission not found");
 		}
 		res.status(200).json({
 			success: true,
-			message: "Permission updated successfully",
+			message: "Permission deleted successfully",
 			data: permission,
 		});
 	} catch (err) {
 		res.status(500).json({ success: false, message: err.message });
 	}
 });
-
-// Delete a permission for a space and user
-router.delete(
-	"/spaces/:spaceId/users/:userId/permissions",
-	async (req, res) => {
-		const { spaceId, userId } = req.params;
-		try {
-			const permission = await Permission.findOneAndDelete({ spaceId, userId });
-			if (!permission) {
-				throw new Error("Permission not found");
-			}
-			res.status(200).json({
-				success: true,
-				message: "Permission deleted successfully",
-				data: permission,
-			});
-		} catch (err) {
-			res.status(500).json({ success: false, message: err.message });
-		}
-	}
-);
 
 export default router;
